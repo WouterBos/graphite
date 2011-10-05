3c4d0b2ef8d36758a979a468f0c57e426f2e1a67
Wouter Bos <wouter@thebrightlines.com>
2011-10-05 08:38:47 +0200

    Menu demo

---
+++ Website/javascript/graphite.js
@@ -0,0 +1,50 @@
+/**
+ * @fileOverview JavaScript for Graphite demos
+ * @author Wouter Bos, Web developer at Estate Internet (www.estate.nl).
+ * @since 0.1 - 2011-11-05
+ * @version 0.1 - 2011-11-05
+ */
+
+ 
+ 
+ 
+ 
+ 
+/**
+ * @namespace Root namespace for graphite
+ */
+var graphite = {};
+
+
+
+/**
+ * @namespace Extracts code from demo and presents to user.
+ *
+ * @param {Object} root The demo container.
+ */
+graphite.demo = function(root) {
+	
+	// HTML
+	// If demo HTML is found
+		// Get HTML source from DOM
+		// Present HTML in textarea
+	// If no demo HTML was found
+		// Show text: "No HTML used"
+	
+	// CSS
+	// If CSS is used
+		// Get CSS files that have been used  from DOM
+		// Make CSS readable
+		// Present CSS in textarea
+		// Add references to .less files below textarea
+	// If no CSS is used
+		// Show text: "No CSS used"
+	
+	// JAVASCRIPT
+	// Get JavaScript files that have been used from DOM
+	// Get JavaScript code that has been used in the demo page
+	// Present JavaScript in textarea
+	// Add references to JavaScript libraries below textarea
+	
+	// Setup copy links for each textarea
+}
