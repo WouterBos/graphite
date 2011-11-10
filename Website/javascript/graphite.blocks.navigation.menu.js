30c8fe2d56ee80b9139a9b2db2f36d384cb59f26
Wouter Bos <wouter@thebrightlines.com>
2011-11-10 21:15:14 +0100

    enu collapse

--- Website/javascript/graphite.blocks.navigation.menu.js
+++ Website/javascript/graphite.blocks.navigation.menu.js
@@ -1,9 +1,9 @@
 /**
- * @fileOverview Basic functionality for Graphite
+ * @fileOverview Graphite basic menu
  * @author Wouter Bos, Web developer at Estate Internet (www.estate.nl).
  *
- * @since 0.1 - 2011-11-03
- * @version 0.1 - 2011-11-03
+ * @since 0.1 - 2011-11-10
+ * @version 0.1 - 2011-11-10
  */
 
 
@@ -12,30 +12,13 @@
 
 
 
-
 /**
- * @namespace Root namespace for Graphite
- */
-if (typeof(graphite) == 'undefined') {
-  // Only create Graphite object once
-  var graphite = {};
-} else {
-  if (!document.getElementById('GraphiteDemoSite')) {
-    throw Error('Graphite error: root library may only be loaded once.')
-  }
-}
-
-
-
-
-
-/**
- * @namespace Event manager
+ * @namespace Menu functionality
  * @class
  * @since 1.0 - 2010-02-23
  * @version 1.0 - 2010-02-23
  */
-graphite.events = ( function() {
+graphite.blocks.navigation.menu = ( function() {
   /* Start public */
   return {
     /**
@@ -43,12 +26,6 @@ graphite.events = ( function() {
      *
      * @since 1.0 - 2011-11-03
      * @version 1.0 - 2011-11-03
-     * @param {Object} element The element on which the event is placed.
-     * @param {Function} eventFunction The function that has to be linked to
-     *    the event.
-     * @param {String} eventType Name of the event.
-     * @param {Boolean} legacy If this value is false or does not exist,
-     *    the event will be created with "addEventHandler".
      * @example
         graphite.events.addevent(
           document.getElementById('elementId'),
@@ -57,35 +34,42 @@ graphite.events = ( function() {
           true
         );
      */
-    addEvent: function(element, eventFunction, eventType, legacy) {
-      //var error;
-      //error = Estate.Check.VariableType( element, "object" )
-      //if (error != "") throw new Error( error );
-      //error = Estate.Check.VariableType( eventFunction, "function" )
-      //if (error != "") throw new Error( error );
+    initCollapse: function() {
+      // Get the menu root element. We'll use jQuery for brevity
+      var menuListItems = jQuery('.gp_menu.gp_menu_typeHorizontal.gp_menu_typeCollapse li');
+      var cssDelay = new graphite.css.cssDelay(true);
       
-      if (legacy && legacy == true) {
-        if (element instanceof Array) {
-          for (var i = 0; i < element.length; i++) {
-            addLegacyEvent(element[i]);
+      menuListItems.each(function() {
+        cssDelay.addItem(
+          {
+            element: jQuery(this)[0],
+            cssclass: 'gp_hover',
+            eventType: 'mouseover',
+            action: 'add',
+            time: 0
           }
-        } else {
-          addLegacyEvent(element)
-        }
-      } else {
-        element.addEventListener(eventType, eventFunction);
-      }
-
-      function addLegacyEvent(element) {
-        var oldEvent = eval("element.on" + eventType);
-        var eventContentType = eval("typeof element.on" + eventType)
-        
-        if ( eventContentType != 'function' ) {
-          eval("element.on" + eventType + " = eventFunction")
-        } else {
-          eval("element.on" + eventType + " = function(e) { oldEvent(e); eventFunction(e); }")
-        }
-      }
+        );
+        cssDelay.addItem(
+          {
+            element: jQuery(this)[0],
+            cssclass: 'gp_hover',
+            eventType: 'mousemove',
+            action: 'add',
+            time: 0
+          }
+        );
+        cssDelay.addItem(
+          {
+            element: jQuery(this)[0],
+            cssclass: 'gp_hover',
+            eventType: 'mouseout',
+            action: 'remove',
+            time: 1000
+          }
+        );
+      });
+      
+      cssDelay.createEvents();
     }
   }
   /* End public */
