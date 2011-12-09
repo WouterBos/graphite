003ea21f8ee80a5cab3a07e1c294860021c710dc
Wouter Bos (estate) <w.bos@estate.nl>
2011-12-09 14:44:55 +0100

    Send mail

---
+++ Website/App_Code/Graphite.Mail.BodyCreator.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace Graphite.Mail
+{
+    public class BodyCreator
+    {
+        #region "Private Methods"
+
+        /// <summary>
+        /// Loads the content of the file into a string
+        /// </summary>
+        /// <param name="templateLocation"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        private string GetHTMLTemplate(string templateLocation)
+        {
+            string BodyTemplate = System.IO.File.ReadAllText(templateLocation);
+            return BodyTemplate;
+        }
+
+        /// <summary>
+        /// Replaces placeholders in the template string
+        /// </summary>
+        /// <param name="bodyTemplate">String holding the template</param>
+        /// <param name="bodyText">Value to replace the ##BODY## placeholder</param>
+        /// <param name="mailSubject">Value to replace the ##SUBJECT## placeholder</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        private string FillTemplate(string bodyTemplate, string bodyText, string mailSubject)
+        {
+            string FilledTemplate = "";
+
+            FilledTemplate = bodyTemplate.Replace("##BODY##", bodyText);
+            FilledTemplate = FilledTemplate.Replace("##SUBJECT##", mailSubject);
+
+            return FilledTemplate;
+        }
+
+        /// <summary>
+        /// Formats values of the input controls on the control to HTML
+        /// </summary>
+        /// <param name="control"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        private string FormatFormValues(Control control)
+        {
+            ArrayList FormFieldsCollection = new ArrayList();
+            GetFormValuesFromPanel(control, ref FormFieldsCollection);
+            string TableStyle = "border-bottom: 1px dotted #bababa";
+            string ColumnStyleLeft = "border-top: 1px dotted #bababa; " +
+                                     "vertical-align: top; " +
+                                     "padding-top: 2px; " +
+                                     "padding-right: 5px; " +
+                                     "padding-bottom: 2px; " +
+                                     "color: #000000; " +
+                                     "white-space: nowrap; " +
+                                     "font: normal 12px/18px Arial, Helvetica, sans-serif";
+            string ColumnStyleRight = "border-top: 1px dotted #bababa; " +
+                                      "padding-top: 2px; " +
+                                      "padding-bottom: 2px; " +
+                                      "vertical-align: top; " +
+                                      "color: #000000; " +
+                                      "font: normal 12px/18px Arial, Helvetica, sans-serif";
+            System.Text.StringBuilder CormValuesFormatted = new System.Text.StringBuilder();
+            string ItemValue = null;
+
+            CormValuesFormatted.Append("<table border='0' cellspacing='0' cellpadding='0' style='" + TableStyle + "'>\r\n");
+            foreach (UtilityObj Item in FormFieldsCollection)
+            {
+                ItemValue = Item.Value; //Estate.Common.Text.Texttools.HTML2Text(Item.Value);
+                CormValuesFormatted.Append("<tr>");
+                CormValuesFormatted.Append("<td style='" + ColumnStyleLeft + "'><strong>" + FormatFormFieldName(Item.Name) + "</strong></td>\r\n");
+                CormValuesFormatted.Append("<td style='" + ColumnStyleRight + "'>" + ItemValue + "</td>\r\n");
+                CormValuesFormatted.Append("</tr>");
+            }
+            CormValuesFormatted.Append("</table>");
+
+            return CormValuesFormatted.ToString();
+        }
+
+        /// <summary>
+        /// Replaces _ in the fieldname with spaces
+        /// </summary>
+        /// <param name="formFieldName"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        private string FormatFormFieldName(string formFieldName)
+        {
+            string ReturnString = null;
+
+            ReturnString = formFieldName;
+            ReturnString = ReturnString.Replace("_", " ");
+
+            return ReturnString;
+        }
+
+        /// <summary>
+        /// Gets input values from the controls in the control
+        /// </summary>
+        /// <param name="control"></param>
+        /// <returns>Arraylist(Of UtilityObj)</returns>
+        /// <remarks></remarks>
+        private void GetFormValuesFromPanel(Control control, ref ArrayList arrList)
+		{
+			foreach (Control ControlInForm in control.Controls) {
+				if (ControlInForm is TextBox) {
+					arrList.Add(new UtilityObj(ControlInForm.ID, ((TextBox)ControlInForm).Text));
+				} else if (ControlInForm is RadioButtonList) {
+					arrList.Add(new UtilityObj(ControlInForm.ID, ((RadioButtonList)ControlInForm).SelectedValue));
+				} else if (ControlInForm is CheckBox) {
+					CheckBox oCheckBox = (CheckBox)ControlInForm;
+					if (oCheckBox.Checked == true) {
+						arrList.Add(new UtilityObj(oCheckBox.ID, "Ja"));
+					}
+				}
+				if (ControlInForm.HasControls()) {
+					GetFormValuesFromPanel(ControlInForm, ref arrList);
+				}
+			}
+		}
+
+
+        #endregion
+
+        #region "Public Methods"
+        /// <summary>
+        /// Creates a mailbody
+        /// </summary>
+        /// <param name="formPanel"></param>
+        /// <param name="templateLocation"></param>
+        /// <param name="mailSubject"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public string Create(Panel formPanel, string templateLocation, string mailSubject)
+        {
+            string BodyTemplate = null;
+            string BodyText = null;
+            string ReturnBody = null;
+
+            BodyTemplate = GetHTMLTemplate(templateLocation);
+            BodyText = FormatFormValues(formPanel);
+            ReturnBody = FillTemplate(BodyTemplate, BodyText, mailSubject);
+
+            return ReturnBody;
+        }
+        #endregion
+    }
+
+    #region "Key/valuepair object"
+    public class UtilityObj
+    {
+        #region "Private Properties"
+        private string _name;
+        #endregion
+        private string _value;
+
+        #region "Constructor"
+        public UtilityObj(string Name, string Value)
+        {
+            _name = Name;
+            _value = Value;
+        }
+        #endregion
+
+        #region "Public Properties"
+        public string Name
+        {
+            get { return _name; }
+            set { _name = Name; }
+        }
+
+        public string Value
+        {
+            get { return (_value); }
+            set { _value = value; }
+        }
+        #endregion
+    }
+
+    #endregion
+
+}
---
+++ Website/App_Code/Graphite.Mail.HandleForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Diagnostics;
+using System.Web;
+using System.Net.Mail;
+
+
+
+namespace Graphite.Mail
+{
+    public class HandleForm
+    {
+        private string _mailFrom = "";
+        private string _mailTo = "";
+        private string _mailCc = "";
+        private string _mailBcc = "";
+        private string _mailSubject = "";
+        private string _mailBody = "";
+        private string _mailBodyTemplatePath = HttpContext.Current.Server.MapPath("~\\App_Data\\Mail\\Template.html");
+        private string _Attachment;
+        private Panel _FormPanel;
+        private Panel _SendOkPanel;
+        private Panel _SendFailedPanel;
+
+        public string Attachment
+        {
+            get { return _Attachment; }
+            set { _Attachment = value; }
+        }
+        public string From
+        {
+            get { return _mailFrom; }
+            set { _mailFrom = value; }
+        }
+
+        public string To
+        {
+            get { return _mailTo; }
+            set { _mailTo = value; }
+        }
+
+        public string Cc
+        {
+            get { return _mailCc; }
+            set { _mailCc = value; }
+        }
+
+        public string Bcc
+        {
+            get { return _mailBcc; }
+            set { _mailBcc = value; }
+        }
+
+        public string Subject
+        {
+            get { return _mailSubject; }
+            set { _mailSubject = value; }
+        }
+
+        public string Body
+        {
+            get { return _mailBody; }
+            set { _mailBody = value; }
+        }
+
+        public string BodyTemplatePath
+        {
+            get { return _mailBodyTemplatePath; }
+            set { _mailBodyTemplatePath = HttpContext.Current.Server.MapPath(value); }
+        }
+
+        public Panel FormPanel
+        {
+            get { return _FormPanel; }
+            set { _FormPanel = value; }
+        }
+
+        public Panel SendOkPanel
+        {
+            get { return _SendOkPanel; }
+            set { _SendOkPanel = value; }
+        }
+
+        public Panel SendFailedPanel
+        {
+            get { return _SendFailedPanel; }
+            set { _SendFailedPanel = value; }
+        }
+        /// <summary>
+        /// Gets default e-mail address from database
+        /// </summary>
+        /// <param name="datacontext"></param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        private string GetDefaultMailAddress()
+        {
+            return "w.bos@estate.nl";
+        }
+
+        /// <summary>
+        /// Fills _mailbody with the formatted mailbody
+        /// </summary>
+        /// <remarks></remarks>
+        public void CreateBody()
+        {
+            BodyCreator MyBodyCreator = new BodyCreator();
+            _mailBody = MyBodyCreator.Create(_FormPanel, _mailBodyTemplatePath, _mailSubject);
+        }
+
+        /// <summary>
+        /// Sends e-mail
+        /// </summary>
+        /// <remarks></remarks>
+        public void SendMail()
+        {
+            MailMessage Mail = new MailMessage();
+
+            if (string.IsNullOrEmpty(_mailTo))
+            {
+                _mailTo = "wouter@estate.nl";
+            }
+            if (!string.IsNullOrEmpty(_mailCc))
+            {
+                Mail.CC.Add(_mailCc);
+            }
+            if (!string.IsNullOrEmpty(_mailBcc))
+            {
+                Mail.Bcc.Add(_mailBcc);
+            }
+
+            Mail.From = new MailAddress(_mailFrom);
+            Mail.To.Add(_mailTo);
+            Mail.Subject = _mailSubject;
+            Mail.Body = _mailBody;
+            Mail.IsBodyHtml = true;
+
+            if (!string.IsNullOrEmpty(_Attachment) & System.IO.File.Exists(_Attachment))
+            {
+                Attachment attachment = new Attachment(_Attachment);
+                //create the attachment
+                Mail.Attachments.Add(attachment);
+                //add the attachment
+            }
+
+            _FormPanel.Visible = false;
+            _SendOkPanel.Visible = true;
+
+            try
+            {
+                SmtpClient SMTP = new SmtpClient();
+                SMTP.Host = "192.168.1.3";
+                SMTP.Send(Mail);
+            }
+            catch (Exception ex)
+            {
+                _SendOkPanel.Visible = false;
+                _SendFailedPanel.Visible = true;
+            }
+            finally
+            {
+                //
+            }
+        }
+    }
+}
