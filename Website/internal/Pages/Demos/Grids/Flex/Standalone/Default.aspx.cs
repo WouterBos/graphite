d06b3716195ab3f335c7a2da7a24875f65543a25
Wouter Bos <wouter@thebrightlines.com>
2012-01-27 08:01:59 +0100

    Cleaning up the grids

--- Website/internal/Pages/Demos/Grids/Flex/Standalone/Default.aspx.cs
+++ Website/internal/Pages/Demos/Grids/Flex/Standalone/Default.aspx.cs
@@ -14,10 +14,9 @@ public partial class _Default : System.Web.UI.Page
 
     private void GetDemoCode()
     {
-        Graphite.Tools oGraphite = new Graphite.Tools();
         string path = Server.MapPath(Request.ServerVariables["SCRIPT_PATH"]) + "\\";
         path = path.Replace("\\standalone", "");
-        string DemoHtmlSource = oGraphite.getSourceCode("default-external.html", path);
+        string DemoHtmlSource = Graphite.Tools.GetSourceCode("default-external.html", path);
         DemoHtmlSource = DemoHtmlSource.Replace("###GP_TITLE###", this.Title);
         DemoHtml.Text = DemoHtmlSource;
     }
