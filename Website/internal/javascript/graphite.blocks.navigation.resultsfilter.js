e451feb77a120505ceb0f0d8a8e6f238cf66ee12
Wouter Bos <wouter@thebrightlines.com>
2012-06-04 08:30:42 +0200

    Results filter - pager

--- Website/internal/javascript/graphite.blocks.navigation.resultsfilter.js
+++ Website/internal/javascript/graphite.blocks.navigation.resultsfilter.js
@@ -102,7 +102,14 @@ graphite.blocks.navigation.resultsFilter.obj = function(config) {
   if (_config.root.find('.local_log').size() > 0) {
     _config.log = true;
   }
-  var _resultsList = new graphite.blocks.navigation.resultsFilter.list(_config);
+  var _pager = new graphite.blocks.navigation.resultsFilter.pager(
+    _config.controlPaging
+  );
+  var _resultsList = new graphite.blocks.navigation.resultsFilter.list(
+    _config,
+    _pager
+  );
+  _pager.addListBuilder(_resultsList);
   
   // Set events on all form fields
   function setFilterEvents() {
@@ -285,13 +292,17 @@ graphite.blocks.navigation.resultsFilter.obj = function(config) {
  * 
  * @param {Object} config The config object that is provided when calling
  *    graphite.blocks.navigation.resultsFilter.init.
+ * @param {Object} pager Pager instance, created with
+ *    graphite.blocks.navigation.resultsFilter.pager
  */
-graphite.blocks.navigation.resultsFilter.list = function(config) {
+graphite.blocks.navigation.resultsFilter.list = function(config, pager) {
   var _queryData;
   var _sortKey = '';
   var _sortOrder = '';
   var _pageSize = 0;
+  var _pageIndex = 0;
   var _config = config;
+  var _pager = pager;
   var _queryDataKeys = config.queryDataKeys   // The data that has to be fetched
                                              // from the queryData
   
@@ -358,10 +369,12 @@ graphite.blocks.navigation.resultsFilter.list = function(config) {
   
   function buildList() {
     var orderedIndex = createOrderedIndex();
-    orderedIndex = orderedIndex.slice(0, _pageSize);
+    var sliceStart = (_pageIndex * _pageSize);
+    orderedIndex = orderedIndex.slice(sliceStart, sliceStart + _pageSize);
     var results = getResultsData(orderedIndex);
     var listHtml = Mustache.render(_config.listTemplate, results);
     _config.list.html(listHtml);
+    _pager.page(_pageIndex, _queryData.length, _pageSize);
   }
   
   function setDefaultVariables() {
@@ -393,8 +406,9 @@ graphite.blocks.navigation.resultsFilter.list = function(config) {
    * @param {String} sortKey A key name of items in the raw JSON. That key
    *    is used to sort the list.
    * @param {String} sortOrder Sets order type: 'asc' or 'desc'.
+   * @param {String} pageIndex Sets the paging index.
    */
-  this.build = function(queryData, sortKey, sortOrder, pageSize) {
+  this.build = function(queryData, sortKey, sortOrder, pageSize, pageIndex) {
     // Store arguments as local variable
     if (queryData) {
       _queryData = queryData;
@@ -408,6 +422,7 @@ graphite.blocks.navigation.resultsFilter.list = function(config) {
     if (pageSize) {
       _pageSize = pageSize;
     }
+    _pageIndex = (pageIndex) ? pageIndex : 0;
     
     setDefaultVariables();
     
@@ -428,6 +443,9 @@ graphite.blocks.navigation.resultsFilter.list = function(config) {
  * @param {Array} config Global config object.
  */
 graphite.blocks.navigation.resultsFilter.list.controls = (function() {
+  // Return the text after the last dot of a string. That way you get the last
+  // key of a JSON selector Example: function('root.trunk.leaf') will return
+  // the key 'leaf'.
   function orderKeyName(str) {
     var key = str;
     if (key.lastIndexOf('.') >= 0) {
@@ -435,7 +453,8 @@ graphite.blocks.navigation.resultsFilter.list.controls = (function() {
     }
     return key;
   }
-  
+
+  // Make one list order link appear selected.  
   function orderSetActive(selected, config) {
     // Deselect all other items
     config.controlOrder.find('a').not(selected).removeClass('gp_active')
@@ -451,6 +470,7 @@ graphite.blocks.navigation.resultsFilter.list.controls = (function() {
     }
   }
   
+  // Make one page size link appear selected.
   function pageSizeSetActive(selected, config) {
     // Deselect all other items
     config.controlBatch.find('a').not(selected).removeClass('gp_active');
@@ -460,6 +480,9 @@ graphite.blocks.navigation.resultsFilter.list.controls = (function() {
   }
   
   return {
+    /**
+     * Create list order links
+     */
     order: function(resultsList, config) {
       config.controlOrder.html(Mustache.render(config.listOrderTemplate, config));
       config.controlOrder.find('a').on('click', function() {
@@ -475,10 +498,16 @@ graphite.blocks.navigation.resultsFilter.list.controls = (function() {
       });
     },
     
+    /**
+     * Make one list order link appear selected.
+     */
     orderSetActive: function(index, config) {
       orderSetActive(config.controlOrder.find('a:eq(' + index + ')'), config);
     },
 
+    /**
+     * Create page size links
+     */
     pageSize: function(resultsList, config) {
       config.controlBatch.html(Mustache.render(config.listBatchTemplate, config));
       config.controlBatch.find('a').on('click', function() {
@@ -490,15 +519,131 @@ graphite.blocks.navigation.resultsFilter.list.controls = (function() {
       });
     },
 
+    /**
+     * Make one page size link appear selected.
+     */
     pageSizeSetActive: function(index, config) {
       pageSizeSetActive(config.controlBatch.find('a:eq(' + index + ')'), config);
     },
     
+    /**
+     * Return the text after the last dot of a string. That way you get the last
+     * key of a JSON selector Example: function('root.trunk.leaf') will return
+     * the key 'leaf'.
+     */
     orderKeyName: function(str) {
       return orderKeyName(str);
     }
   }
 })();
+
+
+
+
+
+
+/**
+ * @namespace Pager manager for the results list
+ * 
+ * @param {Object} pagerContainer The element (as jQuery object) where the
+ *    pager will be created in.
+ */
+graphite.blocks.navigation.resultsFilter.pager = function(pagerContainer, labels) {
+  var _resultsList = {};
+  var _index = 0;
+  var _total = 0;
+  var _pageSize = 2;
+  var visibleRange = 2;
+  var _class = {
+   first: 'local_first', 
+   last: 'local_last',
+   previous: 'local_previous', 
+   next: 'local_next',
+   number: 'local_number' 
+  }
+  var _labels = {
+    first: 'first',
+    last: 'last',
+    previous: 'previous',
+    next: 'next'
+  };
+  jQuery.extend(_labels, labels);
+  var _pagerContainer = pagerContainer;
+  
+  function buildPager() {
+    var html = '';
+    var hrefVoid = 'href="javascript:void(0);"'
+    
+    html += '<a ' + hrefVoid + ' class="' + _class.first + '">' + _labels.first + '</a>\n';
+    html += '<a ' + hrefVoid + ' class="' + _class.previous + '">' + _labels.previous + '</a>\n';
+    html += numbers(hrefVoid);
+    html += '<a ' + hrefVoid + ' class="' + _class.next + '">' + _labels.next + '</a>\n';
+    html += '<a ' + hrefVoid + ' class="' + _class.last + '">' + _labels.last + '</a>\n';
+    
+    _pagerContainer.html(html);
+    setEvents();
+  }
+  
+  function setEvents() {
+    _pagerContainer.find('.' + _class.first).click(function() {
+      _resultsList.build(false, false, false, false, 0);
+    });
+    _pagerContainer.find('.' + _class.last).click(function() {
+      _resultsList.build(false, false, false, false, _total);
+    });
+    _pagerContainer.find('.' + _class.previous).click(function() {
+      _resultsList.build(false, false, false, false, _index - 1);
+    });
+    _pagerContainer.find('.' + _class.next).click(function() {
+      _resultsList.build(false, false, false, false, _index + 1);
+    });
+    _pagerContainer.find('.' + _class.number).click(function() {
+      _resultsList.build(false, false, false, false, jQuery(this).html());
+    });
+  }
+  
+  function numbers(hrefVoid) {
+    var html = '';
+    var start = _index - visibleRange;
+    if (start < 0) {
+      start = 0;
+    }
+    var end = start + (visibleRange * 2)
+    if (end >= _total) {
+      end = _total;
+    }
+    for (i = start; i < end; i++) {
+      html += '<a ' + hrefVoid;
+      if (i == _index) {
+        html += ' class="gp_active"'
+      }
+      html += '>' + (i + 1) + '</a>\n';
+    }
+    return html;
+  }
+  
+  
+  
+  this.page = function(index, total, pageSize, relative) {
+    if (relative == true) {
+      _index += index;
+    } else {
+      _index = index;
+    }
+    if (pageSize) {
+      _pageSize = pageSize;
+    }
+    _total = total;
+    
+    buildPager();
+  }
+  
+  this.addListBuilder = function(resultsList) {
+    if (resultsList) {
+      _resultsList = resultsList;
+    }
+  }
+}
 
 
 
